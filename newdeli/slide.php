7cd3080730fbb047a08a61ace7ce551847a7ee8e
Yusril Octa <54065000+yusrilocta@users.noreply.github.com>
2023-10-04 16:09:18 +0700

    Add files via upload

---
+++ newdeli/slide.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Freelancer - Start Bootstrap Theme</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    <style>
+        /* Gaya untuk membuat carousel full screen */
+        .carousel {
+            position: relative;
+            width: 100%;
+            height: 100vh; /* 100% tinggi viewport */
+        }
+        .carousel-inner {
+            height: 100%;
+        }
+        .carousel-item {
+            position: relative;
+            height: 100%;
+        }
+        .carousel-item img {
+            object-fit: cover;
+            width: 100%;
+            height: 100%;
+        }
+        .carousel-control-prev, .carousel-control-next {
+            width: auto;
+            font-size: 2rem;
+        }
+        /* Gaya untuk tombol navigasi di tengah bawah */
+        .carousel-control-prev, .carousel-control-next {
+            top: auto;
+            bottom: 0;
+            background-color: rgba(0, 0, 0, 0.3);
+            border: none;
+        }
+        /* Gaya untuk teks di tengah bawah */
+        .carousel-caption {
+            position: absolute;
+            height: 100vh;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            background-color: rgba(0, 0, 0, 0.5);
+            padding: 10px;
+            text-align: center;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+
+<div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <!-- Slide 1 -->
+        <div class="carousel-item active">
+            <img src="https://via.placeholder.com/1920x1080?text=Slide+1" alt="Slide 1">
+            <div class="carousel-caption">
+            <header class="masthead bg-primary text-white text-center vh-100">
+            <div class="container d-flex align-items-center flex-column">
+                <!-- Masthead Avatar Image-->
+                <img class="masthead-avatar mb-5" src="assets/img/pem.png" alt="..." />
+                <!-- Masthead Heading-->
+                <h1 class="masthead-heading text-uppercase mb-0">Pempek Naka</h1>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- Masthead Subheading-->
+                <p class="masthead-subheading font-weight-light mb-0">Asli Palembang - Terbuat dari Ikan Gabus</p>
+            </div>
+        </header>
+            </div>
+        </div>
+
+        <!-- Slide 2 -->
+        <div class="carousel-item">
+            <div class="carousel-caption">
+            <section class="page-section bg-primary text-white mb-0 vh-100" id="portfolio">
+            <div class="container">
+                <!-- Portfolio Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Pempek Asli palembang</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- Portfolio Grid Items-->
+                <div class="row justify-content-center">
+                <img class="img-fluid" src="assets/img/pempek.jpg" alt="..." />
+                </div>
+            </div>
+        </section>
+            </div>
+        </div>
+
+        <!-- Slide 3 -->
+        <div class="carousel-item">
+            <img src="https://via.placeholder.com/1920x1080?text=Slide+3" alt="Slide 3">
+            <div class="carousel-caption">
+<section class="page-section bg-primary text-white mb-0 vh-100" id="about">
+            <div class="container">
+<!-- ======= Menu Section ======= -->
+<section id="menu" class="menu">
+<div class="container">
+
+    <div class="section-title">
+        <h2>Paket <span>Pempek Kecil</span></h2>
+    </div>
+
+    <table class="table table-borderless text-center h2 text-secondary">
+        <thead>
+            <tr>
+                <th scope="col">Porsi</th>
+                <th scope="col">Harga</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>30 Pempek Kecil</td>
+                <td>Rp. 135.000</td>
+            </tr>
+            <tr>
+                <td>38 Pempek Kecil</td>
+                <td>Rp. 170.000</td>
+            </tr>
+            <tr>
+                <td>50 Pempek Kecil</td>
+                <td>Rp. 225.000</td>
+            </tr>
+            <tr>
+                <td>63 Pempek Kecil</td>
+                <td>Rp. 280.000</td>
+            </tr>
+            <tr>
+                <td>75 Pempek Kecil</td>
+                <td>Rp. 330.000</td>
+            </tr>
+            <tr>
+                <td>100 Pempek Kecil</td>
+                <td>Rp. 450.000</td>
+            </tr>
+            <tr>
+                <td>130 Pempek Kecil</td>
+                <td>Rp. 565.000</td>
+            </tr>
+        </tbody>
+    </table>
+
+</div>
+    </section><!-- End Menu Section -->
+            </div>
+        </section>
+            </div>
+        </div>
+                <!-- Slide 3 -->
+                <div class="carousel-item">
+            <img src="https://via.placeholder.com/1920x1080?text=Slide+3" alt="Slide 3">
+            <div class="carousel-caption">
+<section class="page-section bg-primary text-white mb-0 vh-100" id="about">
+            <div class="container">
+                <!-- About Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-white">Pesan Sekarang</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- About Section Content-->
+                <div class="row">
+                    <div class="col-lg-4 ms-auto"><p class="lead">Pempek biasanya disajikan dengan saus cuko, yang terbuat dari campuran cuka, gula merah, garam, bawang putih, dan cabai. </p></div>
+                    <div class="col-lg-4 me-auto"><p class="lead">Pempek Palembang memiliki berbagai variasi, seperti pempek kapal selam (yang berisi telur rebus), pempek lenjer (pipih dan panjang), pempek adaan (bulat kecil), dan lain-lain. Makanan ini sangat populer di Indonesia dan memiliki rasa yang unik dan gurih.</p></div>
+                </div>
+                <!-- About Section Button-->
+                <div class="text-center mt-4">
+                    <a class="btn btn-xl bg-warning btn-outline-light text-dark" href="https://startbootstrap.com/theme/freelancer/">
+                        <i class="fab fa-whatsapp me-2"></i>
+                        Hubungi Kami
+                    </a>
+                </div>
+            </div>
+        </section>
+            </div>
+        </div>
+    </div>
+
+    <!-- Tombol Navigasi -->
+    <a class="carousel-control-prev" href="#myCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </a>
+</div>
+
+<!-- Sertakan Bootstrap JavaScript --><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
